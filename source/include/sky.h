7045ea2b7a88723c22fa5eb2b66a36af3b711044
ndx <bg.physique@gmail.com>
2018-11-07 09:16:03 +0100

    working on skydome

---
+++ source/include/sky.h
@@ -0,0 +1,14 @@
+#ifndef SKY_H
+#define SKY_H
+
+class SkyDome
+{
+private:
+
+
+public:
+    SkyDome();
+    virtual ~SkyDome();
+};
+
+#endif // SKY_H
---
+++ source/src/sky.cpp
@@ -0,0 +1,11 @@
+#include "sky.h"
+
+SkyDome::SkyDome()
+{
+
+}
+
+SkyDome::~SkyDome()
+{
+
+}
