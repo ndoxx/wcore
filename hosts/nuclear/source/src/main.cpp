2782bb4be671269d5c818b1246ef0143751f0fe7
ndx <bg.physique@gmail.com>
2018-12-15 17:23:28 +0100

    optimized Simpson integration

--- hosts/nuclear/source/src/main.cpp
+++ hosts/nuclear/source/src/main.cpp
@@ -6,12 +6,7 @@
 #include "gaussian.h"
 #include "numeric.h"
 
-using namespace std::placeholders;
 using namespace wcore::math;
-
-float x_2(float x) { return x*x; }
-float x_3(float x) { return x*x*x; }
-float sine(float x) { return sin(x); }
 
 int main(int argc, char** argv)
 {
--- source/src/numeric.cpp
+++ source/src/numeric.cpp
@@ -5,30 +5,23 @@ namespace wcore::math
 
 float integrate_simpson(std::function<float (float)> f, float lb, float ub, uint32_t subintervals)
 {
-    // * Simpson's rule integration is more accurate if we subdivide the interval of integration
-    float x[subintervals+1],
-          y[subintervals+1];
-    float h = (ub-lb)/subintervals; //calculate width of subintervals
+    // * Simpson's rule is more accurate if we subdivide the interval of integration
+    float h        = (ub-lb)/subintervals, // width of subintervals
+          sum_odd  = 0.0f,                 // sum of odd subinterval contributions
+          sum_even = 0.0f,                 // sum of even subinterval contributions
+          y0       = f(lb),                // f value at lower bound
+          yn       = f(ub);                // f value at upper bound
 
-    //loop to evaluate x0,...xn and y0,...yn
-    for(int ii=0; ii<subintervals+1; ++ii)
+    // loop to evaluate intermediary sums
+    for(int ii=1; ii<subintervals; ++ii)
     {
-        x[ii]=lb+ii*h;
-        y[ii]=f(x[ii]);
+        float yy = f(lb + ii*h); // evaluate y_ii
+        // sum of odd terms go into sum_odd and sum of even terms go into sum_even
+        ((ii%2)?sum_odd:sum_even) += yy;
     }
 
-    //loop to evaluate (y1+y3+y5+...+yn-1)
-    float sum4 = 0.0f;
-    for(int ii=1; ii<subintervals; ii+=2)
-        sum4 += y[ii];
-
-    //loop to evaluate (y2+y4+y6+...+yn-2)
-    float sum2 = 0.0f;
-    for(int ii=2; ii<subintervals-1; ii+=2)
-        sum2 += y[ii];
-
-    //h/3*[y0+yn+4*(y1+y3+y5+...+yn-1)+2*(y2+y4+y6+...+yn-2)]
-    return h/3.0f*(y[0] + y[subintervals] + 2.0f*sum2 + 4.0f*sum4);
+    // h/3*[y0+yn+4*(y1+y3+y5+...+yn-1)+2*(y2+y4+y6+...+yn-2)]
+    return h/3.0f*(y0 + yn + 4.0f*sum_odd + 2.0f*sum_even);
 }
 
 } // namespace wcore::math
